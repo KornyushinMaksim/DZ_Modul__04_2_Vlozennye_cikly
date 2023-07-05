771a06584cfcc0c3ea607d0dd5058d51cf54645b
KornyushinMaksim <ma.korny@yandex.ru>
2023-07-05 21:16:49 +0300

    classWork1

--- src/Task4.java
+++ src/Task4.java
@@ -4,12 +4,11 @@
 //        Высота каждой фигуры должны задаваться с клавиатуры.
 //        Можно использовать вывод только одной «звёздочки» в System.out.print(“*”).
 //
-//        Задание 3
-//        Равнобедренный треугольник: заполненный и пустой.
+//        Ромб: заполненный и пустой.
 
 import java.util.Scanner;
 
-public class Task3 {
+public class Task4 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите высоту фигуры: ");
@@ -17,17 +16,15 @@ public class Task3 {
 
         System.out.println("\nЗаполненный");
         for (int i = 0; i < a; i++){
-            for (int j = 0; j < a; j++){
-                if (i >= j && i + j >= a - 1) System.out.print(" * ");
+            for (int j = 0; j < a * 2; j++){
+                if (j >= a - i && j <= a + i) System.out.print(" * ");
                 else System.out.print("   ");
             }
             System.out.println();
         }
-
-        System.out.println("\nПустой");     //доделать
-        for (int i = 0; i < a; i++){
-            for (int j = 0; j < a; j++){
-                if (i == j && i >= a / 2 || i + j == a - 1 && i >= a / 2 || i == a - 1) System.out.print(" * ");
+        for (int i = 1; i < a; i++){
+            for (int j = 0; j < a * 2; j++){
+                if (i <= a - j && i >= a + j) System.out.print(" * ");
                 else System.out.print("   ");
             }
             System.out.println();
